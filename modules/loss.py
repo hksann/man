3e5c123c136b84b50d8dae04e76f38c9a7b37e1f
hksann <96939706+hksann@users.noreply.github.com>
2023-11-08 18:38:15 +0800

    Update loss.py

--- modules/loss.py
+++ modules/loss.py
@@ -1,19 +1,33 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class LanguageModelCriterion(nn.Module):
     def __init__(self):
         super(LanguageModelCriterion, self).__init__()
 
     def forward(self, input, target, mask):
-        # truncate to the same size
+        # 调整 target 和 mask 以匹配 input 的维度
         target = target[:, :input.size(1)]
         mask = mask[:, :input.size(1)]
-        output = -input.gather(2, target.long().unsqueeze(2)).squeeze(2) * mask
-        output = torch.sum(output) / torch.sum(mask)
-        return output
 
+        if input.dim() == 3:
+            # 三维情况（训练阶段）
+            target = target.unsqueeze(-1)  # 在最后一个维度扩展 target
+            output = -input.gather(2, target.long()).squeeze(2) * mask
+        elif input.dim() == 2:
+            # 二维情况（评估阶段）
+            if mask.size(1) != input.size(1):
+                if mask.size(1) < input.size(1):
+                    padding_size = input.size(1) - mask.size(1)
+                    mask = F.pad(mask, (0, padding_size), 'constant', 0)
+                else:
+                    mask = mask[:, :input.size(1)]
+
+            output = -input * mask
+
+        output = torch.sum(output) / (torch.sum(mask) + 1e-9)  # 为防止除以零的情况
+        return output
 
 def compute_loss(output, reports_ids, reports_masks):
     criterion = LanguageModelCriterion()
