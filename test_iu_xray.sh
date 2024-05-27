8365888653e280f7deaa993ab04b405b81a8c77e
hksann <96939706+hksann@users.noreply.github.com>
2024-05-27 17:57:18 +0800

    Update test_iu_xray.sh

--- test_iu_xray.sh
+++ test_iu_xray.sh
@@ -1,21 +1,4 @@
 python main_test.py \
     --image_dir /kaggle/input/images/iu_xray/images \
     --ann_path /kaggle/input/images/iu_xray/annotation.json \
-    --dataset_name iu_xray \
-    --max_seq_length 60 \
-    --threshold 3 \
-    --epochs 100 \
-    --batch_size 16 \
-    --lr_ve 1e-4 \
-    --lr_ed 5e-4 \
-    --step_size 10 \
-    --gamma 0.8 \
-    --num_layers 3 \
-    --topk 32 \
-    --cmm_size 2048 \
-    --cmm_dim 512 \
-    --seed 7580 \
-    --beam_size 3 \
-    --save_dir results/iu_xray/ \
-    --log_period 50 \
-    --load /kaggle/input/model-r2gencmn-best/model_best.pth
+    --load "/kaggle/input/model-191/model_best (0.191).pth"
