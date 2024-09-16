b4c28b433613c5b5353d0e8a50bc63e6fed31585
hksann <96939706+hksann@users.noreply.github.com>
2024-09-17 02:47:03 +0800

    Update trainer.py

--- modules/trainer.py
+++ modules/trainer.py
@@ -10,7 +10,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-from .base_cmn import BaseCMN 
+from .base_cmn import BaseCMN
 from torch.cuda.amp import autocast, GradScaler
 from googletrans import Translator
 from .loss import compute_loss
@@ -223,7 +223,7 @@ class Trainer(BaseTrainer):
         for batch_idx, (images_id, images, reports_ids, reports_masks) in enumerate(self.train_dataloader):
             images, reports_ids, reports_masks = images.to(self.device), reports_ids.to(self.device), reports_masks.to(self.device)
 
-            with autocast('cuda'):
+            with autocast():
                 output = self.model(images, reports_ids, mode='train')
                 loss = self.criterion(output, reports_ids, reports_masks)
 
